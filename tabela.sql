bb43cb012f3341b5b6d6fc1ea62695b70c8e764c
Alexandre <asilvestre786@gmail.com>
2021-11-23 23:25:42 -0300

    sql atualizado

--- tabela.sql
+++ tabela.sql
@@ -3,6 +3,7 @@ CREATE TABLE jogos
     nome character varying NOT NULL,
     genero character varying NOT NULL,
     imagem character varying NOT NULL,
-    id_jogos character varying NOT NULL,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     descritivo bigint NOT NULL,
+    PRIMARY KEY(id)
 )
