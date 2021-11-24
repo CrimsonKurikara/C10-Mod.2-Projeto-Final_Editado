48389643efb7928274c35096187eae09a70e1cfb
CrimsonKurikara <gabriel_silva.2016@outlook.com>
2021-11-24 20:13:21 -0300

    edit

--- index.js
+++ index.js
@@ -2,19 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 require('dotenv').config()
-const crud = require("./controller/crud")
+
+let mensagem = ""
 
 const port = process.env.PORT || 3000;
 const db = require('./model/database')
-const jogos = require('./model/jogo');
+const Jogos = require('./model/jogo');
 
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded())
 
-// rota do crud
-app.use("/", crud);
+app.get('/' , async (req, res) =>{
+    let jogos = await Jogos.findAll();
+    res.render("index", {mensagem, jogos});
+});
+
+app.get('/cadastro' , (req, res) => {
+    res.render('cadastro');
+});
+
+app.post('/cadastro', async(req, res) =>{
+    const {nome, genero, imagem, descritivo} = req.body
+    await Jogos.create({
+        nome,
+        genero,
+        imagem,
+        descritivo,
+    })
+    mensagem = `O jogo ${nome} foi adicionado`
+    res.redirect('/')
+})
 
 db.conectado();
 app.listen(port, () =>console.log(`Servidor rodando em http://localhost:${port}`));
